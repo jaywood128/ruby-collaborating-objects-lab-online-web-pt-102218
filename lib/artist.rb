c47be4bffee4c46ba5e3e026e52ee94bf7418883
Johnathon Wood <jaywood128@gmail.com>
2018-12-13 17:31:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,6 @@ class Artist
   end
 
   def add_song(song)
-
-    song.artist = self
     @songs << song
   end
 
--- lib/song.rb
+++ lib/song.rb
@@ -11,11 +11,13 @@ class Song
   end
 
   def self.new_by_filename(filename)
+
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
-   song = Song.new(song_name)
    artist = Artist.find_or_create_by_name(artist_name)
+  song = Song.new(song_name)
    artist.add_song(song)
+   song.artist = artist
    song
  end
 
